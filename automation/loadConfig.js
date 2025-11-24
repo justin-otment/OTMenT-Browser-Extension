0f7b456423b46baed6dc8f4229cde0f6451bd6d0
justin-otment <justin@otm-enterprises.com>
2025-11-24 12:05:42 +0800

    Update loadConfig.js

--- automation/loadConfig.js
+++ automation/loadConfig.js
@@ -2,7 +2,7 @@ import fs from "fs/promises";
 import path from "path";
 
 export async function loadNodeConfig() {
-  const baseDir = path.resolve("./GitHub-Onyot"); // <-- your extension subfolder
+  const baseDir = path.resolve("./GitHub-Onyot");
 
   const config = JSON.parse(await fs.readFile(path.join(baseDir, "config.json"), "utf8"));
   const sa     = JSON.parse(await fs.readFile(path.join(baseDir, "service-account.json"), "utf8"));
